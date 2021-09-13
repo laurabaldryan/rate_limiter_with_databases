26c8cda7ced7f963f1e5ff17998cb3760c0e2554
Laura <63228952+laurabaldryan@users.noreply.github.com>
2021-09-13 14:43:38 -0700

    Update rate_limiter_with_databases.c

--- rate_limiter_with_databases.c
+++ rate_limiter_with_databases.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
     exit(1);
   }
 
-  if (mysql_real_connect(con, "localhost", "laura", "Password123#@!",
+  if (mysql_real_connect(con, "localhost", "laura", "#######",
   "card", 0, NULL, 0) == NULL)
   {
     finish_with_error(con);
